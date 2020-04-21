c8426d0e9776cda9386b81cb41bb0fbd6b3a3bfc
wxsatellite <1453085314@qq.com>
2020-04-21 22:33:24 +0800

    判断json的key或者value是否一致

---
+++ util/check.py
@@ -0,0 +1,40 @@
+from deepdiff import DeepDiff
+
+from util.read_data import ReadStandardApiReturn
+
+
+class CheckJson:
+    read = None
+
+    def __init__(self):
+        if self.read is None:
+            self.read = ReadStandardApiReturn()
+
+    def check_api_key_return(self, url, result):
+        standard = self.read.get_standard(url)
+        return self.key_is_ok(standard, result)
+
+    def check_api_value_return(self, url, result):
+        standard = self.read.get_standard(url)
+        return self.value_is_ok(standard, result)
+
+    # 返回的结构是否一致
+    @classmethod
+    def key_is_ok(cls, dict1, dict2):
+        if not isinstance(dict1, dict) or not isinstance(dict2, dict):
+            return False
+        res = DeepDiff(dict1, dict2, ignore_order=True).to_dict()
+        return res.get("dictionary_item_added") is None and res.get("dictionary_item_removed") is None
+
+    # 返回的值是否一致
+    @classmethod
+    def value_is_ok(cls, dict1, dict2):
+        if not isinstance(dict1, dict) or not  isinstance(dict2,dict):
+            return False
+        res = DeepDiff(dict1, dict2, ignore_order=True).to_dict()
+        return res.get("values_changed") is None
+
+
+if __name__ == "__main__":
+    check = CheckJson()
+    print(check.check_api_key_return("login", {"code": -1, "message": "用户名或者密码错误", "data": None}))
--- util/read_data.py
+++ util/read_data.py
@@ -2,13 +2,15 @@ import os
 import json
 
 
-class HandleErrorCodeMessageType:
+class ReadErrorCodeMessage:
     base_path = os.path.dirname(__file__)
 
-    def __init__(self, file_name):
+    def __init__(self, file_name=None):
+        if file_name is None:
+            file_name = "error_code_message.json"
         file_path = self.base_path + "/../data/" + file_name
         with open(file_path, encoding="utf-8") as f:
-            self.data = json.loads(f)
+            self.data = json.loads(f.read())
 
     def get_value(self, url):
         return self.data.get(url)
@@ -18,3 +20,17 @@ class HandleErrorCodeMessageType:
         if info is None:
             return ""
         return info.get(code)
+
+
+class ReadStandardApiReturn:
+    base_path = os.path.dirname(__file__)
+
+    def __init__(self, file_name=None):
+        if file_name is None:
+            file_name = "standard_api_return.json"
+        file_path = self.base_path + "/../data/" + file_name
+        with open(file_path, encoding="utf-8") as f:
+            self.data = json.loads(f.read())
+
+    def get_standard(self, url):
+        return self.data.get(url)
