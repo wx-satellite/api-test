9293e1f1ecbc7378a4b99cf3baf0d3f3984407e6
wxsatellite <1453085314@qq.com>
2020-04-24 00:09:43 +0800

    回写excel

--- run.py
+++ run.py
@@ -1,5 +1,10 @@
-from util.config import Config
+from util.excel import Excel
 
 if __name__ == "__main__":
-    c = Config()
-    print(c.get_value("host"))
+
+    excel = Excel()
+
+    rows = excel.get_row_nums()
+
+    for i in range(2, rows + 1):
+        row_value = excel.get_row_value_from_target_sheet(i)
--- util/excel.py
+++ util/excel.py
@@ -1,7 +1,9 @@
 from openpyxl import load_workbook
 import os
 
+
 class Excel:
+
     def __init__(self, path=None):
         if path is None:
             self.path = os.path.dirname(__file__) + "/../case/base.xlsx"
@@ -18,15 +20,27 @@ class Excel:
             row_values.append(v.value)
         return row_values
 
-    def get_row_nums(self,target_index=0):
+    # 回写了值之后需要执行 save 方法
+    def set_cell_value_from_target_sheet(self, row_index, cell_index, value,target_index=0):
+        # self.instance.active
+        sheet = self.get_target_sheet(target_index)
+        sheet.cell(row_index, cell_index, value)
+
+    def save(self, path=None):
+        if path is None:
+            path = self.path
+        self.instance.save(path)
+
+    def get_row_nums(self, target_index=0):
         sheet = self.get_target_sheet(target_index)
         return sheet.max_row
 
 
 if __name__ == "__main__":
     handleExcel = Excel()
-
+    handleExcel.set_cell_value_from_target_sheet(2, 3, "测试啊测试")
     # 行是从 1 开始的，但是又要去掉表头，所以从 2 开始
     for i in range(2, handleExcel.get_row_nums() + 1):
         # 获取每一行的值
         print(handleExcel.get_row_value_from_target_sheet(i))
+
